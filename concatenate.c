1233946ad1602374e349aea2656006146acd6e86
SUVAM SAMANTA <suvamsamanta06@gmail.com>
2025-06-04 21:55:32 +0530

    add file

---
+++ concatenate.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    char arr1[100]="suvam";
+    char arr2[]="samanta";
+    int i=0,j=0;
+    while (arr1[i]!='\0')
+    {
+        i++;
+    }
+    while (arr2[j]!='\0')
+    {
+        arr1[i] = arr2[j];
+        j++;
+        i++;
+    }
+    arr1[i]='\0';
+    int k=0;
+    printf("%s",arr1);
+    return 0;
+}
